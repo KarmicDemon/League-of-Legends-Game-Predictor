b3932a1b9cf5453033f2adf178b6b2ea4a3a530c
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-04-10 23:35:54 -0400

    Harder to get dataset sigh

--- download_train_test_set.py
+++ download_train_test_set.py
@@ -1,1 +1,21 @@
 ### This file contains the download functions and split functions
+import json
+import requests as req
+
+
+### Download seed data since Riot is hella stingy
+### Returns one big dataset
+def download_data():
+    data = []
+
+    for i in range(1, 11):
+        url = 'https://s3-us-west-1.amazonaws.com/riot-developer-portal/' + \
+            'seed-data/matches' + str(i) + '.json'
+        print('Currently Downloading Part %d of 10 of Seed Match Dataset' % i)
+        data.append(req.get(url).json())
+
+    return data
+
+
+if __name__ == '__main__':
+    download_data()
--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,15 @@
 ### This file contains preprocessing methods
+class Game(object):
+    def __init__(self, obj_game):
+        ### from data object get teams, champions, summoners,
+        pass
+
+    def __str__(self):
+        pass
+
+class Player(object):
+    def __init__(self, obj_player):
+        ### from player object
+        pass
+
+    def __str__(self):
