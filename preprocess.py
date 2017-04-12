a07004e7475a60aa08f6bdc6e7c9da016b4cc0ef
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-04-12 15:10:07 -0400

    Preprocessed data now must think

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 ### This file contains preprocessing classes
+from download import download_champion_shit, download_data
 
 class Match(object):
     def __init__(self, tup_list):
@@ -9,21 +10,24 @@ class Match(object):
         i_to_c, _ = download_champion_shit()
 
 ### returns new proccessed champs
+### each in ret is [[10x (team, champion_id)] , winning_team]
 def turn_data_to_process():
     data = download_data()
     i_to_c = download_champion_shit()[0]
 
     participants = [x['participants'] for x in data]
+    winners = [0 if x['teams'][0]['winner'] else 1 for x in data]
 
     ret = []
-    for p in participants:
+    for p, w in zip(participants, winners):
         _list = []
         for p2 in p:
             _list.append(((1 if p2['teamId'] == 200 else 0), \
-                i_to_c[p2['championId']]))
-        ret.append(_list)
+                p2['championId']))
+        ret.append((_list, w))
 
     print(ret)
+    return ret
 
 ### retuns preprocess champs to match
 def turn_process_to_match():
