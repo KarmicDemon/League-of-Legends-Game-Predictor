a61b869a96ae968cee316ca0bb49f3a0e31424b5
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-04-11 21:55:56 -0400

    Created champion to int/int to champion

--- download_train_test_set.py
+++ download_train_test_set.py
@@ -2,20 +2,25 @@
 import json
 import requests as req
 import pickle
+import toml
 
 from os.path import isfile
 from pickle import dump, load
 
+int_to_champion_file = 'int_to_champion.p'
+champion_to_int_file = 'champion_to_int.p'
 seed_data_download_file = 'seed_data.p'
+settings_toml = 'settings.toml'
 
 ### Download seed data since Riot is hella stingy
 ### Returns one big dataset
 def download_data():
-    data = None
-
     if isfile(seed_data_download_file):
         data = load(open(seed_data_download_file, 'rb'))
+        return data
     else:
+        data == None
+
         for i in range(1, 11):
             url = 'https://s3-us-west-1.amazonaws.com/riot-developer-portal/' \
                 + 'seed-data/matches' + str(i) + '.json'
@@ -31,10 +36,29 @@ def download_data():
 
         ### Save data to file so we never have to download again
         dump(data, open(seed_data_download_file, 'wb'))
+        return data
 
-    return data
+### returns int_to_champion, champion_to_int
+def download_champion_shit():
+    if isfile(int_to_champion_file) and isfile(champion_to_int_file):
+        return load(open(int_to_champion_file, 'rb')), \
+            load(open(champion_to_int_file, 'rb'))
+    else:
+        _settings = toml.load(settings_toml)
+        url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?' + \
+            'api_key=' + _settings['riot_api_key']
+        json_req = req.get(url).json()['data']
+
+        champions = [x for x, _ in json_req.items()]
+        champions = sorted(champions)
+        champion_to_int = { champ : idx for idx, champ in enumerate(champions) }
+        int_to_champion = { idx : champ for champ, idx in champion_to_int.items() }
+
+        dump(champion_to_int, open(champion_to_int_file, 'wb'))
+        dump(int_to_champion, open(int_to_champion_file, 'wb'))
+
+        return int_to_champion, champion_to_int
 
 
 if __name__ == '__main__':
-    data = download_data()
-    print(data[1]['timeline'])
+    download_champion_shit()
--- nn.py
+++ nn.py
@@ -1,1 +1,3 @@
 ### This file will be where the nn is trained and saves it to file
+import numpy as np
+import tensorflow as tf
