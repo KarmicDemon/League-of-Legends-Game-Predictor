9c1983b344a086334ff45247e8e7b62fc6ca0cce
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-04-28 20:10:32 -0400

    I htink I know the problem but too annoyed

--- nn.py
+++ nn.py
@@ -1,3 +1,61 @@
 ### This file will be where the nn is trained and saves it to file
 import numpy as np
 import tensorflow as tf
+
+from preprocess import get_nn_x_y
+
+nn_data = get_nn_x_y()
+
+#define hyperparamters
+batch_size = 200
+complexity = 25
+epochs = 10
+learning_rate = .01
+num_feature_inputs = 268
+
+# create network
+_inputs = tf.placeholder(tf.float32, [None, num_feature_inputs],
+    name = 'inputs')
+_labels = tf.placeholder(tf.float32, [200], name = 'labels')
+
+biases = {
+    'layer_one' : tf.Variable(tf.random_normal([complexity])),
+    'layer_two' : tf.Variable(tf.random_normal([1]))
+}
+
+weights = {
+    'layer_one' : tf.Variable(tf.random_normal([num_feature_inputs,
+        complexity])),
+    'layer_two' : tf.Variable(tf.random_normal([complexity, 1]))
+}
+
+## layer with sigmoid activation
+nn = tf.add(tf.matmul(_inputs, weights['layer_one']), biases['layer_one'])
+nn = tf.nn.relu(nn)
+nn = tf.add(tf.matmul(nn, weights['layer_two']), biases['layer_two'])
+nn = tf.nn.sigmoid(nn)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = nn,
+    labels = _labels))
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as s:
+    s.run(init)
+
+    for e in range(1, epochs + 1):
+        _cost = 0
+        for i in range(nn_data.num_examples // batch_size):
+            _x, _y = nn_data.get_batch(batch_size)
+
+            _, c = s.run([optimizer, cost], feed_dict = {
+                _inputs : _x,
+                _labels : _y
+            })
+
+            _cost += c / total_batch
+
+        print('Epoch:', epoch + 1, '| average cost =', '.04f' % avg_cost)
+
+    print('Done')
--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,12 @@
 ### This file contains preprocessing classes
+import numpy as np
+
 from download import download_champion_shit, download_data
 
-class Match(object):
-    def __init__(self, tup_list):
-        self.team_one = [x for x in tup_list if x[0] == 0]
-        self.team_two = [x for x in tup_list if x[1] == 0]
-
-    def __str__(self):
-        i_to_c, _ = download_champion_shit()
+def arg_find(arg, _list):
+    for idx, val in enumerate(_list):
+        if val == arg:
+            return idx
 
 ### returns new proccessed champs
 ### each in ret is [[10x (team, champion_id)] , winning_team]
@@ -16,21 +15,73 @@ def turn_data_to_process():
     i_to_c = download_champion_shit()[0]
 
     participants = [x['participants'] for x in data]
-    winners = [0 if x['teams'][0]['winner'] else 1 for x in data]
+    targets = [0 if x['teams'][0]['winner'] else 1 for x in data]
 
-    ret = []
-    for p, w in zip(participants, winners):
+    pre_ret = []
+    features = []
+
+    arg_list = sorted(list(i_to_c.copy().keys()))
+    num_champs = len(list(i_to_c.copy().keys()))
+
+    for p in participants:
         _list = []
         for p2 in p:
-            _list.append(((1 if p2['teamId'] == 200 else 0), \
-                p2['championId']))
-        ret.append((_list, w))
+            _list.append(arg_find(int(p2['championId']), arg_list))
+        pre_ret.append(_list)
 
-    print(ret)
-    return ret
+    for p in pre_ret:
+        hi = p[:5]
+        pi = p[5:]
 
-### retuns preprocess champs to match
-def turn_process_to_match():
-    proc = turn_data_to_process()
+        team_one = [0] * num_champs
+        team_two = [0] * num_champs
+
+
+        for _ in hi:
+            team_one[(_ - 1)] = 1
+
+        for _ in pi:
+            team_one[(_ - 1)] = 1
+
+        yo = team_one + team_two
+        features.append(yo)
+
+    # return features, targets
+    return BatchService(features, targets)
+
+
+### create batch object
+def get_nn_x_y():
+    return turn_data_to_process()
+
+
+
+class BatchService(object):
+    def __init__(self, inputs, outputs):
+        self.data_inputs = inputs
+        self.data_outputs = outputs
+        self.num_examples = len(self.data_inputs)
+
+        self.index = 0
+
+    def get_batch(self, batch_size):
+        start = self.index
+        end = batch_size + self.index
+
+        if (end > self.num_examples):
+            self.index = batch_size
+            start = 0
+            end = batch_size
+
+        self.index = end
+
+        x = np.array(self.data_inputs[start:end])
+        y = np.array(self.data_outputs[start:end])
+
+        #y = np.transpose(y)
+
+        return x, y
+
+if __name__ == '__main__':
+    # turn_data_to_process()
     pass
-    #return [Match(x) for x in proc]
